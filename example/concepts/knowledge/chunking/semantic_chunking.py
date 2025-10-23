62957baf635549740791c952054ade005e6cd636
startzeng <1369866181@qq.com>
2025-10-23 19:37:32 +0800

    feat(knowledge): 更新语义分块示例并升级依赖版本
    - 注释掉原有的 PDF 内容添加逻辑，便于调试和测试
    - 修改 Agent 的指令提示，优化查询响应行为
    - 将查询问题从“炒蛋需要注意什么?”更改为“椰奶鸡汤如何做?”
    - 升级 agno依赖版本从2.1.8 到 2.2.0
    - 升级 psycopg 依赖版本从3.2.10 到 3.2.11

--- example/concepts/knowledge/chunking/semantic_chunking.py
+++ example/concepts/knowledge/chunking/semantic_chunking.py
@@ -16,23 +16,26 @@ vector_db = Milvus(
 knowledge = Knowledge(
     vector_db=vector_db,
 )
-asyncio.run(knowledge.add_content_async(
-    url="https://agno-public.s3.amazonaws.com/recipes/ThaiRecipes.pdf",
-    reader=PDFReader(
-        name="Semantic Chunking Reader",
-        chunking_strategy=SemanticChunking(
-            embedder=TEXT_EMBEDDING_V4,
-            similarity_threshold=0.5,
-        ),
-    ),
-))
+# asyncio.run(knowledge.add_content_async(
+#     url="https://agno-public.s3.amazonaws.com/recipes/ThaiRecipes.pdf",
+#     # reader=PDFReader(
+#     #     name="Semantic Chunking Reader",
+#     #     chunking_strategy=SemanticChunking(
+#     #         embedder=TEXT_EMBEDDING_V4,
+#     #         similarity_threshold=0.5,
+#     #     ),
+#     # ),
+# ))
 
 agent = Agent(
     model=QWEN3_MAX,
+    instructions="""
+    查询知识库回答问题,知识库没有回答一句歉意的话,不废话
+    """,
     knowledge=knowledge,
     search_knowledge=True,
     debug_level=2,
     debug_mode=True,
 )
 
-agent.print_response("炒蛋需要注意什么?", markdown=True)
+agent.print_response("椰奶鸡汤如何做?", markdown=True)
