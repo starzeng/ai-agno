8dfbad8cf411bdedd277d061d92a4bdad04fc6fe
startzeng <1369866181@qq.com>
2025-10-23 20:41:13 +0800

    feat(knowledge): 添加文档分块处理示例
    - 新增 recipes_document_chunking.py 文件用于演示文档分块功能
    - 配置 Milvus 向量数据库和知识库
    - 使用 PDFReader 读取并分块处理 PDF 文档
    - 实现基于知识库的问答代理
    - 添加查询示例以测试文档分块效果

---
+++ example/concepts/knowledge/chunking/recipes_document_chunking.py
@@ -0,0 +1,37 @@
+import asyncio
+
+from agno.agent import Agent
+from agno.knowledge.chunking.document import DocumentChunking
+from agno.knowledge.knowledge import Knowledge
+from agno.knowledge.reader.pdf_reader import PDFReader
+from agno.vectordb.milvus import Milvus
+
+from models.models import TEXT_EMBEDDING_V4, QWEN3_MAX
+
+vector_db = Milvus(
+    collection="recipes_document_chunking",
+    embedder=TEXT_EMBEDDING_V4,
+
+)
+knowledge = Knowledge(
+    vector_db=vector_db
+)
+
+asyncio.run(knowledge.add_content_async(
+    path="./大数据风控实战课程.pdf",
+    reader=PDFReader(
+        name="Document Chunking Reader",
+        chunking_strategy=DocumentChunking(),
+    ),
+))
+
+agent = Agent(
+    model=QWEN3_MAX,
+    instructions="""查询知识库回答问题,知识库没有回答一句歉意的话,不废话""",
+    knowledge=knowledge,
+    search_knowledge=True,
+    debug_mode=True,
+    debug_level=2,
+)
+
+agent.print_response("鸡汤怎么做?", markdown=True)
